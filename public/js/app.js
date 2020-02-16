f12ebe5c47fe4e191b4c8ceff39f22798d49f165
illarionov-school <lavrikanec@gmail.com>
2020-02-16 12:25:05 +0900

    different api on env

--- public/js/app.js
+++ public/js/app.js
@@ -2281,9 +2281,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\interactor\interactor\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\interactor\interactor\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\interactor\interactor\resources\sass\vendor.scss */"./resources/sass/vendor.scss");
+__webpack_require__(/*! D:\interactor\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\interactor\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\interactor\resources\sass\vendor.scss */"./resources/sass/vendor.scss");
 
 
 /***/ })
