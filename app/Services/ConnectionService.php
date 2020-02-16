bd38e95d1e59fc648b5c9087903d9fccda505ddf
illarionov-school <lavrikanec@gmail.com>
2020-02-16 16:15:27 +0900

    bug fix

--- app/Services/ConnectionService.php
+++ app/Services/ConnectionService.php
@@ -10,13 +10,13 @@ use Illuminate\Http\Request;
 class ConnectionService {
 	public function store($data) {
 		$connection = new Connection();
-        $connection->episode_id = $data('episode_id');
-        $connection->entry_id = $data('entry_id');
-        $connection->out_id = $data('out_id');
+        $connection->episode_id = $data['episode_id'];
+        $connection->entry_id = $data['entry_id'];
+        $connection->out_id = $data['out_id'];
         $connection->meta = '{}';
         $connection->save();
-        
-        return Episode::find($data('episode_id'))->connections; 
+
+        return Episode::find($data['episode_id'])->connections; 
 	}
 	
 }
