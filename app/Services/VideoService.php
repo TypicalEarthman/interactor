12fe1fb706a64d63ded7aac69507feeef6df4c91
TypicalEarthman <amazing216man@gmail.com>
2020-02-16 16:58:04 +0900

    set root

--- app/Services/VideoService.php
+++ app/Services/VideoService.php
@@ -10,40 +10,46 @@ use Illuminate\Support\Facades\Storage;
 
 class VideoService {
 	public function store($data) {
-
+                
+        
 		
-        $episode_id = $data["episode_id"];
-        $project_id = $data['project_id'];
-        $video = new Video();
-        $video->name = $data['name'];
+                $episode_id = $data["episode_id"];
+                $project_id = $data['project_id'];
+                $video = new Video();
+                $video->name = $data['name'];
 
-        if(Auth::check()) {
-			$video->user_id = Auth::user()->id;
-		}
+                if(Auth::check()) {
+                        $video->user_id = Auth::user()->id;
+                }
 
-        
-        
-		$filename = $data["filename"];
-        $video->filename = $filename;
-        $video->episode_id = $episode_id;
-        $video->project_id = $project_id;
-        $video->meta = '{}';
+                
+                $filename = $data["filename"];
+                $video->filename = $filename;
+                $video->episode_id = $episode_id;
+                $video->project_id = $project_id;
+                $video->meta = '{}';
+                
+                if(isset($data["video"])) {
+                        $path = Storage::putFileAs("project/{$project_id}/episode/{$episode_id}", $data["video"], $filename);
+                        
+                } else {
+                        $path = $data["url"];
+                }
 
-        if(isset($data["video"])) {
-        	$path = Storage::putFileAs("project/{$project_id}/episode/{$episode_id}", $data["video"], $filename);
-        	
-        } else {
-        	$path = $data["url"];
-        }
+                
+                $video->url = "/storage/{$path}";
+                //$video->filename = $data["video"]->hashName;
 
-        
-        $video->url = "/storage/{$path}";
-        //$video->filename = $data["video"]->hashName;
-
-        
-        $video->save();
-
-        return $video;
+                
+                $video->save();
+                
+                $episode = Episode::where('id', $episode_id)->first();
+                $episode_videos = $episode->videos;
+                if(count($episode_videos) == 1) {
+                        //вставить set_root и передать туды  $episode_videos->first()->id
+                }
+                return $video;
+                
 	}
 	
 }
